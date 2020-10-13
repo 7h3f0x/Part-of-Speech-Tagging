9bba3a8431ab8f6003c63c01f8d0eac1474d27ef
Shubhang Tripathi <stripathi1@cs.iitr.ac.in>
2020-10-13 10:48:02 +0530

    add reporting of top 10 most frequent words and tags

--- pos_tagger.py
+++ pos_tagger.py
@@ -8,11 +8,19 @@ import os
 dirname = "./tag-list"
 TRAINING_LISTS = list()
 
+
 def print_as_table(dictionary: dict, first_col_name: str):
     print("-"*57)
     print(f"| {first_col_name:>30} | {'count':>20} |")
     print("-"*57)
     for key, value in sorted(dictionary.items(), key=lambda x:x[0]):
+        print(f"| {key:>30} | {value:20} |")
+    print("-"*57)
+
+
+def report_top_n(dictionary:dict, n=10):
+    print("-"*57)
+    for key, value in sorted(dictionary.items(), key=lambda x:x[1], reverse=True)[:n]:
         print(f"| {key:>30} | {value:20} |")
     print("-"*57)
 
@@ -31,9 +39,10 @@ def write_to_file(fname, data):
         write_data = ''
         for val in data:
             write_data += f"{val}\n"
-        
+
         with open(fname, 'w') as f:
             f.write(write_data)
+
 
 def parse_single_xml(xml_file):
     tree = ET.parse(xml_file)
@@ -46,7 +55,7 @@ def parse_single_xml(xml_file):
 
         for tag in tags:
             word_tag_list.append(f"{word}_{tag}")
-    
+
     TRAINING_LISTS.append(word_tag_list)    # We keep a list of all the word_tag lists generate
     # Let's dump it to a file now
     fname = f"{dirname}/{xml_file.split('/')[-1].replace('.xml','')}"
@@ -71,22 +80,17 @@ def main():
         parse_single_xml(fname)
 
     for word_tag_list in TRAINING_LISTS:
-        
+
         for word_tag in word_tag_list:
-            word, tag = word_tag.split('_') 
+            word, tag = word_tag.split('_')
             WORD_COUNT[word] = WORD_COUNT.get(word, 0) + 1
             WORD_TAG_COUNT[f"{word}_{tag}"] = WORD_TAG_COUNT.get(f"{word}_{tag}", 0) + 1
             TAG_COUNT[tag] = TAG_COUNT.get(tag, 0) + 1
 
-    write_to_file("word_count", WORD_COUNT)
-    write_to_file("tag_count", TAG_COUNT)
-    write_to_file("word_tag_count", WORD_TAG_COUNT)
-
-    print("Finished")
-
-    print_as_table(WORD_TAG_COUNT, "word_tag")
-    print_as_table(WORD_COUNT, "word")
-    print_as_table(TAG_COUNT, "tag")
+    print("Top 10 most frequent words:")
+    report_top_n(WORD_COUNT)
+    print("\nTop 10 most frequent tags:")
+    report_top_n(TAG_COUNT)
 
 if __name__ == "__main__":
-    main()    
+    main()

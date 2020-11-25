31add777247bd45047a2b0e6e178e95dd9f704c4
Shubhang Tripathi <stripathi1@cs.iitr.ac.in>
2020-11-25 14:24:56 +0530

    fixed some errors which were happening previous time, implemented add 1 smoothening, made some optimizations, made code more modular

--- HMM.py
+++ HMM.py
@@ -1,21 +1,24 @@
 import glob
 import xml.etree.ElementTree as ET
+from decimal import Decimal
+from typing import Iterator
 import pickle
 import os
-from typing import Iterator
 
 
 WORD_TAG_DICT =dict()
 WORD_DICT = dict()
 TAG_DICT = dict()
 TAG_TRANSITION = dict()
-start_count = 0
+start_count = Decimal(0)
 
 
 def parse_sentence(sentence: list, train: bool) -> list:
     global TAG_TRANSITION
     prev_tags = ["^"]
     word_list = list()
+    if list(sentence)[0].tag == 'hi':
+        sentence = list(sentence)[0]
     for word in sentence:
         if word.tag != "mw":
             try:
@@ -40,17 +43,21 @@ def parse_sentence(sentence: list, train: bool) -> list:
     return word_list
 
 
-def parse_single_xml(xml_fname: str, train=False) -> list:
+def parse_single_xml(xml_fname: str, train=False) -> Iterator:
     tree = ET.parse(xml_fname)
     sentences = tree.findall(".//s") # get all sentences
 
-    sentence_list = list()
+    # sentence_list = list()
     for sentence in sentences:
-        sentence_list.append(parse_sentence(sentence, train))
-    return sentence_list
+        tmp = parse_sentence(sentence, train)
+        if not tmp:
+            print(sentence.get('n'))
+            exit(1)
+        yield tmp
+    # return sentence_list
 
 
-def train():
+def train(train_files_list : list):
     global WORD_TAG_DICT
     global WORD_DICT
     global TAG_DICT
@@ -62,7 +69,7 @@ def train():
             WORD_TAG_DICT, WORD_DICT, TAG_DICT, TAG_TRANSITION, start_count = pickle.load(f)
         return
 
-    for fname in glob.glob("Train-corups/A*/*.xml"):
+    for fname in train_files_list:
         sentence_list = parse_single_xml(fname, train=True)
         for word_list in sentence_list:
             for word, tags in word_list:
@@ -71,27 +78,43 @@ def train():
                     TAG_DICT[tag] = TAG_DICT.get(tag, 0) + 1
                     WORD_TAG_DICT[f"{word}_{tag}"] = WORD_TAG_DICT.get(f"{word}_{tag}", 0) + 1
 
-    start_count = sum(TAG_TRANSITION.values())
+    start_count = Decimal(sum(TAG_TRANSITION.values()))
     with open('./cache', 'wb') as f:
         pickle.dump([WORD_TAG_DICT, WORD_DICT, TAG_DICT, TAG_TRANSITION, start_count], f)
 
 
-def probability_tag_tag(tag: str, prev_tag: str) -> float:
+def probability_tag_tag(tag: str, prev_tag: str) -> Decimal:
     if prev_tag == "^":
-        return TAG_TRANSITION.get(f"{tag}_{prev_tag}", 0) / start_count
+        # start probabilities
+        return Decimal(TAG_TRANSITION.get(f"{tag}_{prev_tag}", 0)) / start_count
     else:
-        return TAG_TRANSITION.get(f"{tag}_{prev_tag}", 0) / TAG_DICT.get(prev_tag, 0)
+        # transition probabilities
+        return Decimal(TAG_TRANSITION.get(f"{tag}_{prev_tag}", 0)) / Decimal(TAG_DICT.get(prev_tag, 0))
 
 
-def probability_word_tag(word: str, tag: str) -> float:
-    tmp = WORD_TAG_DICT.get(f"{word}_{tag}", 0)
-    if tmp == 0:
-        return 1 / TAG_DICT.get(tag, 0)
-    return  tmp / TAG_DICT.get(tag, 0)
+def probability_word_tag(word: str, tag: str) -> Decimal:
+    # add 1 smoothening
+    return ( Decimal(WORD_TAG_DICT.get(f"{word}_{tag}", 0)) + Decimal(1) ) / ( Decimal(TAG_DICT.get(tag, 0)) + Decimal(len(TAG_DICT)) )
+
 
 def viterbi(sentence: list) -> list:
-    probability_matrix = [{key: 0.0 for key in TAG_DICT} for _ in range(len(sentence))]
-    back_ptr = [{key: None for key in TAG_DICT} for _ in range(len(sentence))]
+    assert len(sentence) > 0
+
+    # probability_matrix = [{key: Decimal(0.0) for key in TAG_DICT} for _ in range(len(sentence))]
+    # back_ptr = [{key: None for key in TAG_DICT} for _ in range(len(sentence))]
+
+    probability_matrix = list()
+    back_ptr = list()
+
+    for _ in range(len(sentence)):
+        matrix_temp = dict()
+        back_tmp = dict()
+        for key in TAG_DICT:
+            matrix_temp[key] = Decimal(0)
+            back_tmp[key] = None
+        probability_matrix.append(matrix_temp)
+        back_ptr.append(back_tmp)
+
     for tag in TAG_DICT:
         probability_matrix[0][tag] = probability_tag_tag(tag, "^") * probability_word_tag(sentence[0], tag)
         back_ptr[0][tag] = None
@@ -103,7 +126,7 @@ def viterbi(sentence: list) -> list:
                 if  back_probability > probability_matrix[idx][tag]:
                     probability_matrix[idx][tag] = back_probability
                     back_ptr[idx][tag] = prev_tag
-    
+
     best_probability = max(probability_matrix[idx], key=probability_matrix[idx].get)
     sequence = list()
     iterator = best_probability
@@ -112,12 +135,11 @@ def viterbi(sentence: list) -> list:
         iterator = back_ptr[idx][iterator]
         idx -= 1
     sequence.reverse()
+    assert len(sequence) == len(sentence)
     return sequence
 
 
-def main():
-    train()
-
+def hmm(test_files_list: list):
     tag_dict = dict()
     idx = 0
     for key in TAG_DICT.keys():
@@ -134,20 +156,24 @@ def main():
         confusion_matrix.append(t)
 
     word_count = 0
-    for fname in glob.glob("Test-corpus/A*/*.xml"):
+    f_cnt = 0
+    f_skip = 7
+    for fname in test_files_list:
+        # skip initial `f_skip` number of files
+        # FOR TESTING PURPOSE ONLY
+        # TODO : remove this at the end
+        if f_cnt < f_skip:
+            f_cnt += 1
+            continue
+        print(fname)
         for sentence in parse_single_xml(fname):
+            word_count += len(sentence)
             words = [word[0] for word in sentence]
             predicted_tags = viterbi(words)
-            try:
-                for i in range(len(sentence)):
-                    word_count += 1
-                    tag_predicted = predicted_tags[i]
-                    for tag in sentence[i][1]:
-                        confusion_matrix[tag_dict[tag]][tag_dict[tag_predicted]] = confusion_matrix[tag_dict[tag]][tag_dict[tag_predicted]] + 1
-            except:
-                print(sentence)
-                print(predicted_tags)
-
+            for i in range(len(sentence)):
+                tag_predicted = predicted_tags[i]
+                for tag in sentence[i][1]:
+                    confusion_matrix[tag_dict[tag]][tag_dict[tag_predicted]] = confusion_matrix[tag_dict[tag]][tag_dict[tag_predicted]] + 1
             # break
         break
 
@@ -169,5 +195,11 @@ def main():
 
     print(correct_pred, word_count)
 
+def main():
+    train(glob.glob("Train-corups/A*/*.xml"))
+    hmm(glob.glob("Test-corpus/A*/*.xml"))
+
+
 if __name__ == "__main__":
     main()
+
